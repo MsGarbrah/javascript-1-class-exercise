879bbf21b364e63faa191bfb73b376fd97ce44e3
Christabelle <christefani@yahoo.com>
2023-05-13 16:20:07 +0000

    add myscript

---
+++ myscript.js
@@ -0,0 +1,53 @@
+// string variables
+let name = 'Candice';
+
+let color = 'black';
+document.write (color)
+
+let size = 'large';
+
+const country = 'Portugal';
+document.write (country)
+
+const car = "Benz"; 
+document.write (car)
+
+// integer variables
+let page = 25;
+document.write (page);
+
+let position = 15;
+
+let order = 88;
+
+const number = -928263;
+
+const width = 6;
+document.write (width)
+
+// float variables
+let age = 25.5;
+
+let height = 180.5;
+document.write (height)
+
+let weight = 83.5;
+
+const length = 130.5;
+document.write (length)
+
+const  digit= -1673.998776;
+
+// boolean variables
+var tall = true;
+document.write (tall)
+
+var blue = true;
+document.write (blue)
+
+let heavy = false;
+
+let hungry = true;
+
+const tired = true;
+document.write (tired)
